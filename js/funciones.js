8622f045184212bfae0642df95c0359f0fc52bfa
alejandro medici <el.aleyo@gmail.com>
2021-03-25 21:36:15 -0300

    Funciones con y sin parametros

---
+++ js/funciones.js
@@ -0,0 +1,31 @@
+function saludar() {
+    console.log("Hola Coderhouse")
+}
+
+saludar();
+
+function calculadora(pNumero1, pNumero2, pOperation) {
+    let resultado = 0;
+    switch (pOperation) {
+        case "+":
+            resultado = pNumero1 + pNumero2;
+            break;
+        case "-":
+            resultado = pNumero1 - pNumero2;
+            break;
+        default:
+            break;
+    }
+
+    return resultado;
+}
+
+let numero1 = parseInt(prompt("Ingrese numero 1"));
+let numero2 = parseInt(prompt("Ingrese numero 2"));
+let operacion = prompt("Ingrese Operacion: + o - ");
+alert(calculadora(numero1,numero2, operacion));
+
+// Repito el saludar 50 veces
+for (let index = 0; index < 50; index++) {
+    saludar();
+}
